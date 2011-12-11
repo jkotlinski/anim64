430957d875a6cc343ad040a77bde84b0d588c3fd
Johan Kotlinski <kotlinski@gmail.com>
2011-12-11 22:30:21 +0100

    More work on movie screen

--- movie.c
+++ movie.c
@@ -23,13 +23,27 @@ THE SOFTWARE. */
 #include <string.h>
 
 #define FILE_COUNT 24
-#define FILENAME_LENGTH 16
+#define FILENAME_LENGTH 10
 static char filename[FILE_COUNT][FILENAME_LENGTH];
 static unsigned int duration[FILE_COUNT];
+static unsigned char speed[FILE_COUNT];
 
 static char selected_file;
+/* 0 = file name
+ * 1 = duration
+ * 2 = speed */
+static char selected_column;
 
-#define DURATION_X 17
+#define DURATION_X (FILENAME_LENGTH + 1) 
+#define SPEED_X (DURATION_X + 6)
+
+static unsigned char update_color(unsigned char column, unsigned char row) {
+    const unsigned char color = (row != selected_file || column != selected_column)
+        ? (row & 1) ? COLOR_GRAY1 : COLOR_GRAY2
+        : COLOR_GREEN;
+    textcolor(color);
+    return color;
+}
 
 static void show_screen() {
     unsigned char file_it;
@@ -38,22 +52,27 @@ static void show_screen() {
     cputs("file");
     gotoxy(DURATION_X, 0);
     cputs("dur.");
+    gotoxy(SPEED_X, 0);
+    cputs("spd");
+    revers(1);
     for (file_it = 0; file_it < FILE_COUNT; ++file_it) {
         const char y = file_it + 1;
-        const char color = (file_it == selected_file) ? 1 : (file_it & 1 ? COLOR_GRAY1 : COLOR_GRAY2);
-        memset((char*)(0xd800 + y * 40), color, 16);
-        memset((char*)(0x400 + y * 40), 0xa0, 16);
-        memset((char*)(0xd800 + DURATION_X + y * 40), color, 5);
-        memset((char*)(0x400 + DURATION_X + y * 40), 0xa0, 5);
         // Prints file name.
-        textcolor(color);
+        const char color = update_color(0, file_it);
+        memset((char*)(0xd800 + y * 40), color, FILENAME_LENGTH);
+        memset((char*)(0x400 + y * 40), 0xa0, FILENAME_LENGTH);
         gotoxy(0, y);
         cputs(filename[file_it]);
         // Prints duration.
+        update_color(1, file_it);
         gotoxy(DURATION_X, y);
-        revers(1);
         cprintf("%5i", duration[file_it]);
+        // Prints speed.
+        update_color(2, file_it);
+        gotoxy(SPEED_X, y);
+        cprintf("%3i", speed[file_it]);
     }
+    revers(0);
 }
 
 static void init() {
@@ -73,6 +92,10 @@ void edit_movie() {
     show_screen();
     while (1) {
         if (kbhit()) {
+            gotoxy(0, 1);
+            revers(1);
+            gets(NULL);
+            revers(0);
         }
     }
 }
