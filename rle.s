2914616ce515ba43c41252dbd12b0d360ed7d330
Johan Kotlinski <kotlinski@gmail.com>
2011-12-23 20:58:00 +0100

    Flash screen when packing/unpacking.

--- rle.s
+++ rle.s
@@ -50,6 +50,7 @@ destlen:	.res 2		; number of bytes written
 ; read a byte and increment source pointer
 rle_read:
 	lda (src),y
+	sta $d020
 	inc src
 	bne :+
 	inc src + 1
@@ -59,6 +60,7 @@ rle_read:
 ; write a byte and increment destination pointer
 rle_store:
 	sta (dest),y
+	sta $d020
 	inc dest
 	bne :+
 	inc dest + 1
