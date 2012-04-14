d46886fef066053a78ac62acb8ed2e6ddf8a7d4c
Johan Kotlinski <kotlinski@gmail.com>
2012-04-14 19:29:40 +0200

    Add error message in case of disk full

--- movie.c
+++ movie.c
@@ -351,8 +351,12 @@ static void write_onefiler_anims() {
         }
         // Enough space left? +2 for size, another +2 for EOF marker.
         if (heap_end - heap_start >= file_length + 2 + 2) {
-            cbm_write(MY_LFN, &file_length, sizeof(file_length));
-            cbm_write(MY_LFN, &_EDITRAM_LAST__, file_length);
+            if (cbm_write(MY_LFN, &file_length, sizeof(file_length)) <= 0 ||
+                    cbm_write(MY_LFN, &_EDITRAM_LAST__, file_length) <= 0) {
+                textcolor(COLOR_RED);
+                cputs(" disk full?");
+                return;
+            }
             heap_start += file_length + sizeof(file_length);
         } else {
             textcolor(COLOR_RED);
