a8e8e584ad650a8d9048d3381996708261201763
Johan Kotlinski <kotlinski@gmail.com>
2013-01-21 22:41:41 +0100

    New attempt at LZW-77 encoding...

--- lz77/lz77enc.c
+++ lz77/lz77enc.c
@@ -3,78 +3,91 @@
 #include <string.h>
 
 #define ENCODED_FLAG 0x60
+#define SCREEN_SIZE 1501
 
-unsigned char org[0x2000];
-unsigned char packed[0x2000];
+unsigned char org[SCREEN_SIZE];
+unsigned char packed[SCREEN_SIZE];
 
-int write_ptr = 0;
-int read_ptr = 0;
+int write_index = 0;
+int read_index = 0;
 
-/* Backpointer is 3 bytes:
- *   ENCODED_FLAG
- *   bytes back (stored -1, as it will start at least one back)
- *   length (stored -4, as 4 is the minimum useful length)
+/* Backpointer is 3 bytes: ENCODED_FLAG, distance, length.
+ * ENCODED_FLAG bytes are encoded as ENCODED_FLAG, 0xff.
  */
 
+static void write_byte(unsigned char byte) {
+    packed[write_index++] = byte;
+}
 static void copy_byte() {
-    printf("%#x\n", org[read_ptr]);
-    packed[write_ptr++] = org[read_ptr++];
+    char byte = org[read_index++];
+    printf("%x ", 0xff & byte);
+    write_byte(byte);
+    if (byte == ENCODED_FLAG) {
+        write_byte(0xff);
+    }
 }
 
-unsigned char match_ptr;
-unsigned char match_length;
-unsigned char match_found;
-
-static void find_match_length(unsigned char start) {
-    const int start_offset = read_ptr - start - 1;
-    unsigned int read_span = read_ptr - start_offset;
-    unsigned int length = 0;
-    match_found = 0;
-    if (start_offset < 0) {
-        return;
-    }
+static int match_length(int start_index) {
+    int length = 0;
+    int match_index = start_index;
     while (1) {
-        unsigned int cyclic_read_ptr = start_offset + length;
-        while (cyclic_read_ptr >= read_ptr) {
-            cyclic_read_ptr -= read_ptr;
+        assert(match_index >= 0);
+        assert(match_index < SCREEN_SIZE);
+        assert(read_index + length >= 0);
+        assert(read_index + length < SCREEN_SIZE);
+        if (org[match_index] != org[read_index + length]) {
+            break;
         }
-        if (org[cyclic_read_ptr] == org[read_ptr + length]) {
-            if (length == 255 + 4) {
-                break;  // Maximum match made.
-            }
-            ++length;
-        } else {
-            break;  // Can't match longer.
+        ++length;
+        ++match_index;
+        if (match_index == read_index) {
+            match_index = start_index;  // Reached end of window, jump back.
+        }
+        if (length == 0xff) {
+            break;  // Reached max length!
+        }
+        if (read_index + length == SCREEN_SIZE) {
+            break;
         }
     }
-    match_found = (length >= 4);
-    match_length = length - 4;
-}
-
-static void find_pointer() {
-    unsigned char window_ptr_it = 0;
-    while (1) {
-        unsigned char length_it = 0;
-        // Check all window_ptr_it from 0..255
-        find_match_length(window_ptr_it);
-        if (match_found) {
-            printf("%i %i\n", match_ptr - 1, match_length + 4);
-        }
-        if (++window_ptr_it == 0) break;
-    }
+    return length;
 }
 
 static void pack() {
-    copy_byte();
-    find_pointer();
+    while (read_index < SCREEN_SIZE) {
+        int best_match_index = -1;
+        int best_length = -1;
+        for (int match_index = read_index - 1;
+                match_index >= 0 && match_index >= read_index - 0xfe;
+                --match_index) {
+            int length = match_length(match_index);
+            if (length > best_length) {
+                best_match_index = match_index;
+                best_length = length;
+            }
+        }
+        if (best_length > 3) {
+            int distance = read_index - best_match_index;
+            assert(distance < 0xff);
+            printf("[%x %x]", best_match_index, best_length);
+            write_byte(ENCODED_FLAG);
+            write_byte(distance);
+            write_byte(best_length);
+            read_index += best_length;
+        } else {
+            copy_byte();
+        }
+    }
 }
 
 int main() {
-    FILE* f = fopen("004.bin", "rb");
-    int i;
-    fread(org, 1, sizeof(org), f);
+    FILE* f = fopen("wolf7,u", "rb");
+    int read = fread(org, 1, sizeof(org), f);
+    assert(read == SCREEN_SIZE);
     fclose(f);
 
     pack();
+
+    printf("\n%i", write_index);
     return 0;
 }
