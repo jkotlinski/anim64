06cb9172594bf4ae4462727ef093ce18e2178532
Johan Kotlinski <kotlinski@gmail.com>
2013-01-22 21:25:51 +0100

    Hardcode lz77 pack size

--- lz77_dec.c
+++ lz77_dec.c
@@ -1,9 +1,29 @@
+/** Copyright (c) 2013, Johan Kotlinski
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE. */
+
 #include "lz77.h"
 
-const unsigned char* unpack(unsigned char* dst, const unsigned char* src, unsigned int dst_size) {
+const unsigned char* unpack(unsigned char* dst, const unsigned char* src) {
     unsigned int write_index = 0;
 
-    while (write_index < dst_size) {
+    do {
         if (*src == ENCODED_FLAG) {
             if (src[1] == 0xff) {
                 dst[write_index] = ENCODED_FLAG;
@@ -33,6 +53,7 @@ const unsigned char* unpack(unsigned char* dst, const unsigned char* src, unsign
             ++src;
             ++write_index;
         }
-    }
+    } while(write_index < PACK_SIZE);
+
     return src;
 }
--- lz77_enc.c
+++ lz77_enc.c
@@ -1,3 +1,23 @@
+/** Copyright (c) 2013, Johan Kotlinski
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE. */
+
 #include "lz77.h"
 
 #pragma codeseg("EDITCODE")
@@ -9,9 +29,7 @@ unsigned int read_index;
  * ENCODED_FLAG bytes are encoded as ENCODED_FLAG, 0xff.
  */
 
-static unsigned char match_length(const unsigned char* src,
-        unsigned int start_index,
-        int src_size) {
+static unsigned char match_length(const unsigned char* src, unsigned int start_index) {
     unsigned char length = 0;
     unsigned int match_index = start_index;
     while (1) {
@@ -26,24 +44,24 @@ static unsigned char match_length(const unsigned char* src,
         if (length == 0xff) {
             break;  // Reached max length!
         }
-        if (read_index + length == src_size) {
+        if (read_index + length == PACK_SIZE) {
             break;
         }
     }
     return length;
 }
 
-unsigned int pack(unsigned char* dst, const unsigned char* src, unsigned int src_size) {
+unsigned int pack(unsigned char* dst, const unsigned char* src) {
     unsigned int written = 0;
 
     read_index = 0;
 
-    while (read_index < src_size) {
+    while (read_index < PACK_SIZE) {
         unsigned int best_match_index;
         unsigned char best_length = 0;
         int match_index = read_index - 1;
         while (match_index >= 0 && match_index >= read_index - 0xfe) {
-            unsigned char length = match_length(src, match_index, src_size);
+            unsigned char length = match_length(src, match_index);
             if (length > best_length) {
                 best_match_index = match_index;
                 best_length = length;
