08c1535ff5921455ee022e61038840da1e5dc416
Johan Kotlinski <kotlinski@gmail.com>
2013-01-22 19:56:45 +0100

    More refactoring

--- lz77/lz77enc.c
+++ lz77/lz77enc.c
@@ -3,7 +3,6 @@
 #include <string.h>
 
 #define ENCODED_FLAG 0x60
-#define SCREEN_SIZE 1501
 
 int write_index;
 int read_index;
@@ -12,14 +11,14 @@ int read_index;
  * ENCODED_FLAG bytes are encoded as ENCODED_FLAG, 0xff.
  */
 
-static int match_length(const unsigned char* src, int start_index) {
+static int match_length(const unsigned char* src, int start_index, int src_size) {
     int length = 0;
     int match_index = start_index;
     while (1) {
         assert(match_index >= 0);
-        assert(match_index < SCREEN_SIZE);
+        assert(match_index < src_size);
         assert(read_index + length >= 0);
-        assert(read_index + length < SCREEN_SIZE);
+        assert(read_index + length < src_size);
         if (src[match_index] != src[read_index + length]) {
             break;
         }
@@ -31,24 +30,24 @@ static int match_length(const unsigned char* src, int start_index) {
         if (length == 0xff) {
             break;  // Reached max length!
         }
-        if (read_index + length == SCREEN_SIZE) {
+        if (read_index + length == src_size) {
             break;
         }
     }
     return length;
 }
 
-static void pack(unsigned char* dst, const unsigned char* src) {
+static void pack(unsigned char* dst, const unsigned char* src, int src_size) {
     read_index = 0;
     write_index = 0;
 
-    while (read_index < SCREEN_SIZE) {
+    while (read_index < src_size) {
         int best_match_index = -1;
         int best_length = -1;
         for (int match_index = read_index - 1;
                 match_index >= 0 && match_index >= read_index - 0xfe;
                 --match_index) {
-            int length = match_length(src, match_index);
+            int length = match_length(src, match_index, src_size);
             if (length > best_length) {
                 best_match_index = match_index;
                 best_length = length;
@@ -73,11 +72,11 @@ static void pack(unsigned char* dst, const unsigned char* src) {
     }
 }
 
-void unpack(unsigned char* dst, const unsigned char* src) {
+void unpack(unsigned char* dst, const unsigned char* src, int dst_size) {
     read_index = 0;
     write_index = 0;
 
-    while (write_index < SCREEN_SIZE) {
+    while (write_index < dst_size) {
         if (src[read_index] == ENCODED_FLAG) {
             if (src[read_index + 1] == 0xff) {
                 dst[write_index++] = ENCODED_FLAG;
@@ -110,6 +109,8 @@ void unpack(unsigned char* dst, const unsigned char* src) {
     }
 }
 
+#define SCREEN_SIZE 1501
+
 int main() {
     unsigned char original[SCREEN_SIZE];
     unsigned char packed[SCREEN_SIZE];
@@ -120,11 +121,11 @@ int main() {
     assert(read == SCREEN_SIZE);
     fclose(f);
 
-    pack(packed, original);
+    pack(packed, original, SCREEN_SIZE);
     printf("\n%i\n", write_index);
 
     /* Test unpacking. */
-    unpack(unpacked, packed);
+    unpack(unpacked, packed, SCREEN_SIZE);
 
     assert(0 == memcmp(unpacked, original, sizeof(unpacked)));
 
